f135d92468519d0d9b33ccbc75ac5d3bce27a2e2
Sam Borick <sborick@guard1.com>
2017-05-09 13:08:46 -0400

    Add project files.

---
+++ Xamarin Android Barcode Example/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Runtime;
+using Android.Content;
+using Android;
+using Android.Gms.Vision.Barcodes;
+
+namespace Xamarin_Android_Barcode_Example
+{
+    [Activity(Label = "Xamarin_Android_Barcode_Example", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private TextView _textView;
+
+        private const int RC_BARCODE_CAPTURE = 9001;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            _textView = FindViewById<TextView>(Resource.Id.textView1);
+            _textView.Text = "Test";
+
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            button.Click += delegate
+            {
+                Intent intent = new Intent(this, typeof(Scanning.BarcodeCaptureActivity));
+                StartActivityForResult(intent, RC_BARCODE_CAPTURE);
+            };
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == RC_BARCODE_CAPTURE)
+            {
+                if (resultCode == Result.Ok)
+                {
+                    Barcode thisBarcode = (Barcode)data.GetParcelableExtra(Scanning.BarcodeCaptureActivity.BARCODE) ?? null;
+                    _textView.Text = thisBarcode.DisplayValue ?? "Barcode Read Failed";
+                }
+            }
+        }
+    }
+}
---
+++ Xamarin Android Barcode Example/Scanning/BarcodeCaptureActivity.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Xamarin_Android_Barcode_Example.Scanning
+{
+    [Activity(Label = "Xamarin_Android_Barcode_Example", MainLauncher = false, Icon = "@drawable/icon")]
+    class BarcodeCaptureActivity : Activity
+    {
+        public const String BARCODE = "Barcode";
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.BarcodeActivity);
+        }
+    }
+}
